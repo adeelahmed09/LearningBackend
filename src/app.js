22d562785e5301253f36b1a58512dab292d9a0b9
adeelahmed9 <adeel0ahmed10@gmail.com>
2024-12-29 22:19:55 +0500

    add route to user and register

--- src/app.js
+++ src/app.js
@@ -11,14 +11,23 @@ app.use(cors({
 }))
 
 // By this we are limiting user to send only 16kb of json data which will help us to stop server from carshing
+
 app.use(express.json({limit:Limit}))
 
 // By this we are telling express that data from url have different syntax for same thing like for space " ", Some browser change it to "+" and other change it to "%20". For this we are useing urlencoded. "URLENCODED" is enough for mostly work but we are giving "extended:true" to have object in object , Limit have same function as json
+
 app.use(express.urlencoded({extended:true, limit:Limit}))
 
 // It is use to store public assit like pdf, pngs etc
+
 app.use(express.static("public"))
 
 app.use(cookieParser())
+
+// rout import
+
+import userRouter from './routes/user.routes.js'
+app.use("/api/v1/users",userRouter)
+
 
 export {app }
